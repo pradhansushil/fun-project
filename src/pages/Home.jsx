81ee49283f2cb6fe4e646f3176edcfb1a600503b
Sushil Pradhan <sushil.frontenddev@gmail.com>
2026-02-04 07:45:52 +0545

    Initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import AboutMe from "../components/home/AboutMe";
+import Hero from "../components/home/Hero";
+
+export default function Home() {
+  return (
+    <main>
+      <Hero />
+      <AboutMe />
+    </main>
+  );
+}
